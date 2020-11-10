69f3cbf057952f3cc6d013cacc12ac1259c0b7f4
ml-logic-seminar <74162819+ml-logic-seminar@users.noreply.github.com>
2020-11-10 00:27:25 -0500

    Create navigation.js

---
+++ assets/js/navigation.js
@@ -0,0 +1,8 @@
+function minimizeNavBar() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
